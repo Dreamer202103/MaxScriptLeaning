d6bac78c225f6c0fe8b81dcad31d0db16e3740c4
刘大军 <liudajun@yiche.com>
2024-05-23 18:27:19 +0800

    d

--- UnityScript/FindUnuseMaterial.cs
+++ UnityScript/FindUnuseMaterial.cs
@@ -18,13 +18,13 @@ public class FindUnuseMaterial : EditorWindow
     }
 
     private GameObject model;
-    private string path = "请输入文本..."; 
+    private string path = "请输入文本...";
     public void OnGUI()
     {
         GUILayout.Label("请将CarShow预制体拖入下方：");
         model = EditorGUILayout.ObjectField(model, typeof(GameObject), true) as GameObject;
         GUILayout.Label("复制材质球的路径填在下面:");
-        path =EditorGUILayout.TextField("材质球路径:", path);
+        path = EditorGUILayout.TextField("材质球路径:", path);
         if (GUILayout.Button("FindUnuseMaterial"))
         {
             // 获取指定路径下的所有文件和子文件夹
@@ -65,10 +65,10 @@ public class FindUnuseMaterial : EditorWindow
                     }
 
                     bool containsBanana = MateiralsName.Contains(fileNameWithoutExtension);
-                    
+
                     if (!containsBanana)
                     {
-                        
+
                         // File.Delete(path +"/" + fileNameWithoutExtension + ".mat");
                         Debug.Log("删除材质球的名字是：" + fileNameWithoutExtension);
 
@@ -89,18 +89,79 @@ public class FindUnuseMaterial : EditorWindow
         // MateiralsName = null;
         foreach (Renderer renderer in renderers)
         {
-            // 遍历Renderer的materials数组  
-            Material[] materials = renderer.sharedMaterials; // 或者使用renderer.materials来获取实例化的材质  
             
+            // 或者使用renderer.materials来获取实例化的材质 
+            Material[] materials = renderer.sharedMaterials;  
+            // Debug.Log(renderer.name);
 
+            // 遍历Renderer的materials数组  
             foreach (Material material in materials)
             {
                 //Debug.Log("" + material.name);
-                MateiralsName.Add(material.name);
-                //Debug.Log(MateiralsName.Count + "" + material.name);
+                if (material != null)
+                    MateiralsName.Add(material.name);
+                else
+                    Debug.LogError(renderer.name);
 
             }
 
         }
     }
+
+    // public void CollectObjectMaterialsName(GameObject gameObject)
+    // {
+    //     Renderer renderer = gameObject.GetComponent<Renderer>();
+    //     if (renderer != null)
+    //     {
+    //         Material[] materials = renderer.materials;
+    //         if (materials != null)
+    //         {
+    //             foreach (var material in materials)
+    //             {
+    //                 if (material != null)
+    //                 {
+    //                     //Debug.Log("GameObject: " + gameObject.name + " uses Material: " + material.name);
+    //                     MateiralsName.Add(material.name);
+    //                 }
+    //                 else
+    //                 {
+    //                     Debug.LogWarning("GameObject: " + gameObject.name + " has a null material in its material array.");
+    //                 }
+    //             }
+    //         }
+    //         else
+    //         {
+    //             Debug.LogWarning("GameObject: " + gameObject.name + " has a null materials array.");
+    //         }
+    //     }
+
+    //     // 递归检查子对象  
+    //     foreach (Transform child in gameObject.transform)
+    //     {
+    //         CollectObjectMaterialsName(child.gameObject);
+    //     }
+    // }
+
+
+    // public void CollectObjectMaterialsName(GameObject gameObject)
+    // {
+    //     Renderer renderer = gameObject.GetComponent<Renderer>();
+    //     if (renderer != null)
+    //     {
+    //         foreach (var material in renderer.materials)
+    //         {
+    //             if (material != null)
+    //             {
+    //                 // Debug.Log("GameObject: " + gameObject.name + " uses Material: " + material.name);
+    //                 MateiralsName.Add(material.name);
+    //             }
+    //         }
+    //     }
+
+    //     // 递归检查子对象  
+    //     foreach (Transform child in gameObject.transform)
+    //     {
+    //         CollectObjectMaterialsName(child.gameObject);
+    //     }
+    // }
 }
