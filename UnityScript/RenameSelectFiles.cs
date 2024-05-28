6c124038acd880af8e32f67101812ceba4feef24
刘大军 <liudajun@yiche.com>
2024-05-28 09:50:49 +0800

    1

--- UnityScript/RenameSelectFiles.cs
+++ UnityScript/RenameSelectFiles.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class RenameSelectFiles : EditorWindow
 {
     [MenuItem("DreamerTools/RenameSelectionFiles", false, 0)]
     static void RenameSelectionFiles()
     {
-        // var window = GetWindow<RenameSelectFiles>();
-        // window.Show();
-        EditorWindow.GetWindow<RenameSelectFiles>();
+        var window = GetWindow<RenameSelectFiles>();
+        window.Show();
+
     }
 
     private string filesPath = "请输路径...";
@@ -21,20 +22,33 @@ public class RenameSelectFiles : EditorWindow
         filesPath = EditorGUILayout.TextField("重命名文件路径:", filesPath);
         GUILayout.Label("要重名文件的名字填在下面:");
         filesNewName = EditorGUILayout.TextField("重命名文件的名字是:", filesNewName);
-        // GUILayout.Label("要重名文件的路径填在下面：");
-        
 
         if (GUILayout.Button("Rename Selection Files"))
         {
-
             Object[] selecteAssets = Selection.objects;
-
-            for (int i = 0; i < selecteAssets.Length; i++)
+            foreach (var obj in selecteAssets)
             {
-               
-                string filesPath1 = filesPath + "/" + selecteAssets[i].name + ".mat";
-                AssetDatabase.RenameAsset(filesPath1, filesNewName + i);
+    
+                if (obj is UnityEngine.Object unityObject)
+                {
+                    string assetPath = AssetDatabase.GetAssetPath(unityObject);
+                    for (int i = 0; i < selecteAssets.Length; i++)
+                    {  
+                        string extension = Path.GetExtension(assetPath);
+                        Debug.Log("文件的扩展名是：" + extension);
+                        string filesPath1 = filesPath + "/" + selecteAssets[i].name + extension;
+                        AssetDatabase.RenameAsset(filesPath1, filesNewName + i);
+                    }
+                }
+                else
+                {
+                    Debug.Log("选择错误，物体名字：" + obj.name);
+                }
             }
+
+
+
+
         }
     }
 
